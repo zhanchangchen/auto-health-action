48ff4058fbb7f37c997799591afdc0ff35569e1a
zhanchangchen <51755284+zhanchangchen@users.noreply.github.com>
2020-09-28 13:59:00 +0800

    更新多人报餐
    更新多人报餐

--- cypress/integration/auto_report_meals.js
+++ cypress/integration/auto_report_meals.js
@@ -1,31 +1,40 @@
-describe('My First Test', () => {
-  let userAccount = Cypress.env("ZCC_USER_ACCOUNT");
-  let password = Cypress.env("ZCC_PASS_WORD");
-  let accessToken = '';
-  it('login yg-home', function() {
-    cy.request({
-      url:
-        'https://ygjy.ismartwork.cn/ecs/mapp/restful/auth/backstageLoginValidate',
-      method: 'POST',
-      form: false, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
-      headers: {},
-      body: {
-        userAccount: userAccount,
-        password:password
-      }
-    }).then(resp => {
-      expect(resp.status).to.eq(200);
-      accessToken = resp.body.body.ecpToken || '';
+let userAccountStr = Cypress.env("MEALS_USER_ACCOUNT");
+let passwordStr = Cypress.env("MEALS_PASS_WORD");
+
+let userAccount_array = userAccountStr.split(',');
+let password_array = passwordStr.split(',');
+
+userAccount_array.forEach((item, index) => {
+  describe('auto_report_meal_' + index, () => {
+    let accessToken = '';
+    it('login yg-home', () => {
+      cy.request({
+        url:
+          'https://ygjy.ismartwork.cn/ecs/mapp/restful/auth/backstageLoginValidate',
+        method: 'POST',
+        form: false, // indicates the body should be form urlencoded and sets Content-Type: application/x-www-form-urlencoded headers
+        headers: {},
+        body: {
+          userAccount: item,
+          password: password_array[index]
+        }
+      }).then(resp => {
+        expect(resp.status).to.eq(200);
+        accessToken = resp.body.body.ecpToken || '';
+      });
+    });
+
+    it('jump to passpark and submit', () => {
+      const origin_url =
+        'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/';
+      let url = origin_url + '?accessToken=' + accessToken;
+      cy.visit(url);
+      cy.get('.entry-top-right').click();
+      cy.get('.bottom_button').click();
+      cy.get('uni-button[type="primary"]').click();
+      cy.get('.uni-toast__content').should('contain.text', '提交成功');
     });
   });
-
-  it('jump to passpark and submit', () => {
-    const origin_url =
-      'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/';
-    let url = origin_url + '?accessToken=' + accessToken;
-    cy.visit(url);
-    cy.get('.entry-top-right').click();
-    cy.get('.bottom_button').click();
-    cy.get('uni-button[type="primary"]').click();
-  });
 });
+
+
