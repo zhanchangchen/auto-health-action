e9647a9495096b6ae6fbdc2e605d7e9d12b6cdfd
zhanchangchen <51755284+zhanchangchen@users.noreply.github.com>
2020-11-12 19:11:17 +0800

    更新取消循环
    更新取消循环 测试
    优化url

--- cypress/integration/auto_report_meals.js
+++ cypress/integration/auto_report_meals.js
@@ -1,17 +1,13 @@
-let userAccountStr = '04721b8d9fc186c14b0ffc30f2121faa096b2fed525644d7cf4cfebb5b0af6305e840ebdc43764e12eed23991fd980a982c821a390ae6905919b8887844483ec42578ed5662a74f8f31e9a013d66b3b8c40d38bd262908b8e4d9a19b03533e4fe196405522ff46f6aa367d5c628c542affc2ba4c431b2f,04ba024b471d006ca514a00fbaa70c3b74cac10d79a1d0c20ffedee6d8a32778fa5c40bfc0c8e14cdb12cca64af62e2396b15d4f1e466f5319e456304fa4d652f5e922368039813bb9db3a39f760ecfd512b196f12603dff3469cab944847a908b6bec0cbb5053c669247d76aa27ceab292e38eb3a94bfe1c1';
 
 let zccPasswordStr = Cypress.env("ZCC_PASS_WORD");
 let zwpPasswordStr = Cypress.env("ZWP_PASS_WORD");
-let userAccount_array = userAccountStr.split(',');
-let zccUserAccount = userAccount_array[0];
-let zwpUserAccount = userAccount_array[1];
+
+let zccUserAccount = '04281739551a4a21b497fccc38a59d15a4923427f176aa34545dfae05dad2bb89033783740f7a37a1019e225312c3ea20772cbe08e43ff47db2b6e191714724aea3669dc66be74faa6c2d4b11d95ecab2806f785c6547a462293e8ae73c06befcebb83885858964fcdbdfeed63552f87a3bc528df2bf897488';
+let zwpUserAccount = '04ba024b471d006ca514a00fbaa70c3b74cac10d79a1d0c20ffedee6d8a32778fa5c40bfc0c8e14cdb12cca64af62e2396b15d4f1e466f5319e456304fa4d652f5e92236803d9e3fb9cc3c38d277cbf7593e02270521331274d1079050b7a76daadf6158cb58ac8f69d40c9f93c06ccdc92f479a94a58d';
 
 describe('auto_report_meal_zcc', () => {
     let accessToken = '';
     it('login yg-home', () => {
-      let password;
-      password = zccPasswordStr;
-        
       cy.request({
         url:
           'https://ygjy.ismartwork.cn/ecs/mapp/restful/auth/backstageLoginValidate',
@@ -20,7 +16,7 @@ describe('auto_report_meal_zcc', () => {
         headers: {},
         body: {
           userAccount: zccUserAccount,
-          password: password
+          password: zccPasswordStr
         }
       }).then(resp => {
         expect(resp.status).to.eq(200);
@@ -30,21 +26,18 @@ describe('auto_report_meal_zcc', () => {
 
     it('jump to passpark and submit', () => {
       const origin_url =
-        'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/';
+        'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/src/dinner/dinnerStatic';
       let url = origin_url + '?accessToken=' + accessToken;
       cy.visit(url);
-      cy.get('.entry-top-right').click();
       cy.get('.checkLabel').click();
       cy.get('uni-button[type="primary"]').click();
     });
   });
 
-describe('auto_report_meal_zcc', () => {
+describe('auto_report_meal_zwp', () => {
     let accessToken = '';
     it('login yg-home', () => {
-      let password;
-      password = zwpPasswordStr;
-        
+
       cy.request({
         url:
           'https://ygjy.ismartwork.cn/ecs/mapp/restful/auth/backstageLoginValidate',
@@ -53,7 +46,7 @@ describe('auto_report_meal_zcc', () => {
         headers: {},
         body: {
           userAccount: zwpUserAccount,
-          password: password
+          password: zwpPasswordStr
         }
       }).then(resp => {
         expect(resp.status).to.eq(200);
@@ -63,10 +56,10 @@ describe('auto_report_meal_zcc', () => {
 
     it('jump to passpark and submit', () => {
       const origin_url =
-        'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/';
+        'https://ygjy.ismartwork.cn/ecs/mapp/passpark/h5/index.html#/src/dinner/dinnerStatic';
       let url = origin_url + '?accessToken=' + accessToken;
+      console.log(url)
       cy.visit(url);
-      cy.get('.entry-top-right').click();
       cy.get('.checkLabel').click();
       cy.get('uni-button[type="primary"]').click();
     });
